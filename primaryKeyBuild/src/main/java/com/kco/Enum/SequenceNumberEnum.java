8e4d9f4871bb0ae79277965dea4070a0aeb692e9
kco1989 <kco1989@qq.com>
2016-10-26 20:37:40 +0800

    自定义主键生成策略

---
+++ primaryKeyBuild/src/main/java/com/kco/Enum/SequenceNumberEnum.java
@@ -0,0 +1,23 @@
+package com.kco.Enum;
+
+
+import com.kco.bean.SequenceNumberBean;
+
+/**
+ * com.kco.Enum
+ * Created by swlv on 2016/10/25.
+ */
+public enum SequenceNumberEnum {
+    GD(new SequenceNumberBean("GD","工单主键生成策略", 1, 1, 8)),
+    GDD(new SequenceNumberBean("GDD","工单主键生成策略", 1, 1, 8))
+    ;
+
+    private SequenceNumberBean sequenceNumberBean;
+    SequenceNumberEnum(SequenceNumberBean sequenceNumberBean){
+        this.sequenceNumberBean = sequenceNumberBean;
+    }
+
+    public SequenceNumberBean getSequenceNumberBean() {
+        return sequenceNumberBean;
+    }
+}
