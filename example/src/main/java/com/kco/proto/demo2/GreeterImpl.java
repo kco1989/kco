b147c3c6147feee4c6beac10d62d346630f91773
USER-20170410UT\Administrator <kco1989@qq.com>
2017-05-05 18:03:41 +0800

    增加grpc的配置

---
+++ example/src/main/java/com/kco/proto/demo2/GreeterImpl.java
@@ -0,0 +1,21 @@
+package com.kco.proto.demo2;
+
+import io.grpc.stub.StreamObserver;
+import proto.GreeterServiceGrpc;
+import proto.Helloworld;
+
+/**
+ * Created by Administrator on 2017/5/5.
+ */
+public class GreeterImpl extends GreeterServiceGrpc.GreeterServiceImplBase{
+    @Override
+    public void sayHello(Helloworld.HelloRequest request, StreamObserver<Helloworld.HelloReply> responseObserver) {
+        System.out.println("say Hello");
+        String name = request.getName();
+        Helloworld.HelloReply reply = Helloworld.HelloReply.newBuilder()
+                .setMessage("Hello " + name)
+                .build();
+        responseObserver.onNext(reply);
+        responseObserver.onCompleted();
+    }
+}
